941041c08323f297824f3759ff41b938ffce89ec
olatunji <olatunji@localhost.localdomain>
2013-09-09 10:43:33 +0100

    accessing array indexes from a method call is not supported by PHP 5.3.3 which is what the FRMT server is running.

--- src/Flubit/Client/Client.php
+++ src/Flubit/Client/Client.php
@@ -254,11 +254,12 @@ EOH;
         } catch (BadResponseException $e) {
 
             $statusCode = $e->getResponse()->getStatusCode();
-            $msg = (string)$e->getResponse()->xml()['message'];
+            $xml = $e->getResponse()->xml();
+            $msg = (string)$xml['message'];
 
             if ($statusCode === 401) {
 
-                throw new UnauthorizedException($msg, (int)$e->getResponse()->xml()['code']);
+                throw new UnauthorizedException($msg, (int)$xml['code']);
             } else {
 
                 throw new BadMethodCallException($msg, $statusCode);
